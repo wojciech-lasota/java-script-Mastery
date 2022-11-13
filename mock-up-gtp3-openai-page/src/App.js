2ad864da7f8386c39b3ade55e6f07f992c274208
bax <dev.lasota@gmail.com>
2022-11-13 22:55:13 +0100

    nav component added

--- mock-up-gtp3-openai-page/src/App.js
+++ mock-up-gtp3-openai-page/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
-
-import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import "./App.css";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feature,
+  WhatGTP3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>GTP-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGTP3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
